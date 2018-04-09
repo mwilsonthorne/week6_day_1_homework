3412d27c0596703f80d8ed18094b827225525a1d
Matthew Thorne <mwilsonthorne@gmail.com>
2018-04-09 18:15:22 +0100

    Added Calculator runner class

--- calculator/src/main/java/Runner.java
+++ calculator/src/main/java/Runner.java
@@ -1,2 +1,15 @@
 public class Runner {
+
+    public static void main(String[] args) {
+
+        Calculator interger = new Calculator(4, 2);
+
+        System.out.println("For adding the two intergers together = " + interger.getAdd() + " is the correct answer!");
+        System.out.println("For subtracting the two intergers = " + interger.getSubtract() + " is the correct answer!");
+        System.out.println("For multiplying the two intergers together = " + interger.getMultiply() + " is the correct answer!");
+        System.out.println("For dividing the two intergers = " + interger.getDivide() + " is the correct answer!");
+
+    }
+    
 }
+
